45cc9c3f9138bb9f07ca98ba564678dbd2684be4
Pablo <pablo.magarinos@outlook.com>
2024-12-25 18:41:35 +0100

    Funciona

--- Fortran_to_Python/chebyshev_nodes.f90
+++ Fortran_to_Python/chebyshev_nodes.f90
@@ -2,21 +2,64 @@
 !!! Forma de la integral: f(x) / sqrt(1 - x^2)
 !!! https://en.wikipedia.org/wiki/Chebyshev%E2%80%93Gauss_quadrature
 
+
+    function run(n) result(result)
+        
+        implicit none
+        integer, intent(in) :: n
+        
+        real(8) :: result
+        real(8) :: x(n), w(n)
+        real(8) :: gauss
+
+        call chebyshev_nodes(n, x, w)
+        result = gauss(x, w, n)
+
+    end function run
+
+
+
     subroutine chebyshev_nodes(n, x, w)
         ! Calculo de los nodos y pesos de Chebyshev
         implicit none
         integer, intent(in) :: n
         real(8), intent(out) :: x(n), w(n)
-        real(8) :: pi = 3.141592653589793d0
+        real(8), parameter :: pi = 3.141592653589793d0
         integer :: i
 
         do concurrent (i = 1:n)
-            !x(i) = 0.5 * (a + b) + 0.5 * (b - a) * cos((2.0 * i - 1.0) * 3.141592653589793d0 / (2.0 * n))
-            x(i) = cos((2.0 * i - 1.0) * 3.141592653589793d0 / (2.0 * n))
-            w(i) = 3.141592653589793d0 / n ! Peso constante
-
-            !print *, "x(", i, ") = ", x(i), " ; w(", i, ") = ", w(i)
+            x(i) = cos(real(i, 8) / real(n + 1, 8) * pi)
+            w(i) = (pi / real(n + 1, 8)) * sin(real(i, 8) / real(n + 1, 8) * pi)**2
         end do
-            
     end subroutine chebyshev_nodes
+
+
+
+
+    function gauss(x, w, n) result(integral)
+        ! Calculo de la integral con la cuadratura de Gauss-Chebyshev
+        implicit none
+        integer, intent(in) :: n
+        real(8), intent(in) :: x(n), w(n)
+        real(8) :: integral
+        integer :: i
+        real(8) :: f
+
+        integral = 0.0d0
+
+        do i = 1, n
+            integral = integral + w(i) * f(x(i))
+        end do
+    end function gauss
+
+
+
+    function f(x) result(result)
+        ! Definición de la función polinómica f(x) con coeficientes en a
+        implicit none
+        real(8), intent(in) :: x
+        real(8) :: result
+        
+        result = x**2
+    end function f
 
---
+++ Fortran_to_Python/funciona.py
@@ -0,0 +1,6 @@
+import chebyshev_nodes
+
+# Llamar a la función chebyshev con un número de nodos, por ejemplo, 5
+
+resultado = chebyshev_nodes.run(5)
+print(resultado)
